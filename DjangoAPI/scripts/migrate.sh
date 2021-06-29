844695ef2aad0c767925bae00c5d7d657687c074
Shre0226 <86312662+Shre0226@users.noreply.github.com>
2021-06-29 20:07:53 +0530

    Update migrate.sh

--- DjangoAPI/scripts/migrate.sh
+++ DjangoAPI/scripts/migrate.sh
@@ -1,5 +1,5 @@
 cd /var/www/DjangoApp
 source /var/www/DjangoApp -myenv/bin/activate
-DJANGO_SETTINGS_MODULE=DjangoApp/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py makemigrations
-DJANGO_SETTINGS_MODULE=DjangoApp/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py migrate auth
-DJANGO_SETTINGS_MODULE=DjangoApp/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py migrate
+DJANGO_SETTINGS_MODULE=/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py makemigrations
+DJANGO_SETTINGS_MODULE=/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py migrate auth
+DJANGO_SETTINGS_MODULE=/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py migrate
