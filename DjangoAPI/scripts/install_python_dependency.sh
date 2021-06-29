2a3a70f803e95c724739cc1c27f9c5d82f36c488
Shre0226 <86312662+Shre0226@users.noreply.github.com>
2021-06-29 19:40:55 +0530

    Update install_python_dependency.sh

--- DjangoAPI/scripts/install_python_dependency.sh
+++ DjangoAPI/scripts/install_python_dependency.sh
@@ -4,9 +4,16 @@ yum update -y
 yum install httpd
 service httpd start
 chkconfig httpd on
+yum install -y python3
+yum install httpd-devel
+yum install -y mod_wsgi
+pip3 install django==2.1.1
+pip3 install django-cors-headers
+pip3 install djangorestframework
+pip3 install virtualenv
 cd /var/www/
 chown ec2-user:ec2-user /var/www/
 chown ec2-user:ec2-user /var/www/DjangoApp/DjangoAPI/myenv/
 chown ec2-user:ec2-user /var/www/DjangoApp/DjangoAPI/myenv/*
 source /var/www/DjangoApp/DjangoAPI/myenv/bin/activate
-pip install -r /var/www/DjangoApp/DjangoAPI/requirements.txt
+
