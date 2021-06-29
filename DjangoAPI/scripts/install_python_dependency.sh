e578f508ef810e0c2c558ec3f2b7ba48052745d5
Shre0226 <86312662+Shre0226@users.noreply.github.com>
2021-06-29 19:32:28 +0530

    Update install_python_dependency.sh

--- DjangoAPI/scripts/install_python_dependency.sh
+++ DjangoAPI/scripts/install_python_dependency.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-sudo apt-get install libmysqlclient-dev
-sudo yum install -y mysql-devel
-sudo yum install psycopg2
-chown ec2-user:ec2-user /home/ec2-user/www
-chown ec2-user:ec2-user /home/ec2-user/www/code-deploy/DjangoAPI/myenv
-chown ec2-user:ec2-user /home/ec2-user/www/code-deploy/DjangoAPI/myenv/*
-source /home/ec2-user/www/code-deploy/DjangoAPI/myenv/bin/activate
-pip install -r /home/ec2-user/www/code-deploy/DjangoAPI/requirements.txt
+sudo su
+yum update -y
+yum install httpd
+service httpd start
+chkconfig httpd on
+cd /var/www/
+chown ec2-user:ec2-user /var/www/
+chown ec2-user:ec2-user /var/www/DjangoApp/DjangoAPI/myenv/
+chown ec2-user:ec2-user /var/www/DjangoApp/DjangoAPI/myenv/*
+source /var/www/DjangoApp/DjangoAPI/myenv/bin/activate
+pip install -r /var/www/DjangoApp/DjangoAPI/requirements.txt
