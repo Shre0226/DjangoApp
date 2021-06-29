303673ea9b17b61b8a1a52c5be68aebeb48376d2
Shre0226 <86312662+Shre0226@users.noreply.github.com>
2021-06-29 20:19:11 +0530

    Update settings.py

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'DjangoAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-project',
+        'USER': 'django_admin',
+        'PASSWORD': 'athena.s',
+        'HOST': 'django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
+        '
     }
 }
 
