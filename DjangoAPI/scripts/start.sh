a634ed44a1824ca71dda6246cd9144f36780ee17
Shre0226 <86312662+Shre0226@users.noreply.github.com>
2021-06-29 19:44:02 +0530

    Update start.sh

--- DjangoAPI/scripts/start.sh
+++ DjangoAPI/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/www/project/
-source /home/ec2-user/www/code-deploy/DjangoAPI/myenv/bin/activate
+cd /var/www/project/
+source /var/www/DjangoApp/DjangoAPI/myenv/bin/activate
 python manage.py runserver
