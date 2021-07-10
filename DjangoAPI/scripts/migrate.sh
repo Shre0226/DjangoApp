57b82888485a723f5ee11b19bd45b6e769944753
Shre0226 <86312662+Shre0226@users.noreply.github.com>
2021-07-10 13:27:50 +0530

    Update migrate.sh

--- DjangoAPI/scripts/migrate.sh
+++ DjangoAPI/scripts/migrate.sh
@@ -3,3 +3,5 @@ source /var/www/DjangoApp -myenv/bin/activate
 DJANGO_SETTINGS_MODULE=/home/ec2-user/var/www/DjangoApp/DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py makemigrations
 DJANGO_SETTINGS_MODULE=/home/ec2-user/var/www/DjangoApp//DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py migrate auth
 DJANGO_SETTINGS_MODULE=/home/ec2-user/var/www/DjangoApp//DjangoAPI/DjangoAPI/settings RDS_DB_NAME=django-project RDS_DB_USER=django_admin RDS_DB_PASSWD=athena.s RDS_HOST=django-project.cbhwp3yflsbx.us-east-1.rds.amazonaws.com RDS_PORT=3306 ./manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
